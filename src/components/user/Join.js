e6428acb208db87a508aa759c44f6d8153d4e281
1roo <93hanaru@gmail.com>
2023-07-06 17:45:40 +0900

    Join.js에서 이메일까지 완료

--- src/components/user/Join.js
+++ src/components/user/Join.js
@@ -1,11 +1,14 @@
-import React, {useState, useEffect} from 'react';
-
-
+import React, {useState} from 'react';
 import './Join.scss';
 import KakaoAddress from '../util/KakaoAddress';
 
+
 const Join = () => {
-  
+    
+    const [isUserIdAvailable, setIsUserIdAvailable] = useState(false);
+    const [email1, setEmail1] = useState('');
+    const [email2, setEmail2] = useState('');
+    
     //회원가입 입력값 관리용 상태변수
     const [userValue, setUserValue] = useState({
         userId: '',
@@ -16,11 +19,10 @@ const Join = () => {
         userPhone: '',
         userAddr: '',
         userDetailAddr: '',
-        userZip: ''
     });
 
-    //검증메세지 상태변수
-    const [msg, setMsg] = useState({
+    //검증 메세지 상태변수 관리
+    const [message, setMessage] = useState({
         userId: '',
         userPw: '',
         pwCheck: '',
@@ -30,12 +32,12 @@ const Join = () => {
         userPhone: '',
         userAddr: '',
         userDetailAddr: '',
-        userZip: ''
     });
 
-    //검증완료 상태변수
+    //검증완료 상태변수 관리
     const [correct, setCorrect] = useState({
         userId: false,
+        idCheck: false,
         userPw: false,
         pwCheck: false,
         userName: false,
@@ -44,8 +46,165 @@ const Join = () => {
         userPhone: false,
         userAddr: false,
         userDetailAddr: false,
-        userZip: false
     });
+
+    
+    //검증 데이터를 상태변수에 저장하는 함수
+    const saveInputState = ({key, inputValue, msg, flag}) => {       
+
+        inputValue !== 'pass' && setUserValue({
+            ...userValue,
+            [key]: inputValue
+        });
+
+        setMessage({
+            ...message,
+            [key]: msg
+        });
+
+        setCorrect({
+            ...correct,
+            [key]: flag
+        });
+
+    };
+
+
+    //아이디 입력 이벤트 핸들러
+    const idHandler = e => {
+        const idRegex = /^[a-zA-Z0-9]{6,15}$/;
+
+        const inputValue = e.target.value;
+        let msg, flag = false;
+
+        if(!idRegex.test(inputValue)) {
+            msg = '6~15글자 사이의 영문 및 숫자로 작성하세요.';
+        } else if(inputValue){
+            flag = true;
+        }
+        saveInputState({
+            key: 'userId',
+            inputValue,
+            msg,
+            flag
+        });
+    };
+
+    const idCheck = e => {
+        fetch(`/api/checkUserId?userId=${userValue.userId}`)
+            .then(reponse => Response.json())
+            .then(data => {
+                const isAvailable = data.isAvailable;
+                setIsUserIdAvailable(isAvailable);
+                
+            })
+            .catch(error => {
+                console.error('중복 검사 요청 에러:', error);
+            });
+
+        if(isUserIdAvailable) {
+            saveInputState({
+                key: 'idCheck',
+                inputValue: 'pass'
+            })
+            console.log('아이디 제출됨, userId');
+        } else {
+            console.log('중복검사 통과X');
+        }
+    };
+
+    //이름 입력 이벤트 핸들러
+    const nameHandler = e => {
+        const nameRegex = /^[가-힣]{2,5}$/;
+
+        const inputValue = e.target.value;
+        
+        let msg, flag = false; 
+
+        if(!inputValue) {
+            msg = '유저 이름은 필수입니다.';
+        } else if(!nameRegex.test(inputValue)) {
+            msg = '2~5글자 사이의 한글로 작성하세요!';
+        } else {
+            flag = true;
+        }
+        saveInputState({
+            key: 'userName',
+            inputValue,
+            msg,
+            flag
+        });
+    };
+
+    //닉네임 입력 이벤트 핸들러
+    const nickHandler = e => {
+        const inputValue = e.target.value;
+
+        let msg, flag = false;
+
+        saveInputState({
+            key: 'userNick',
+            inputValue
+        });
+    }
+
+    //이메일 입력 이벤트 핸들러
+    const email1Handler = e => {
+        const inputValue = e.target.value;
+        setEmail1(inputValue);
+        emailHandler();
+    }
+
+    const email2Handler = e => {
+        const inputValue = e.target.value;
+        setEmail2(inputValue);
+        emailHandler();
+    }
+
+    const selectedEmailHandler = e => {
+        const inputValue = e.target.value;
+        const $email2 = document.getElementById('email2');
+        if(inputValue !== 'direct') {
+            setEmail2(inputValue);
+            $email2.value = inputValue;
+            $email2.setAttribute('readonly','true');
+            emailHandler();
+            return;
+        }
+        $email2.removeAttribute('readonly','true');
+        $email2.value = '';
+    }
+        
+    const emailHandler = () => {
+        const email1 = document.getElementById('email1').value;
+        const email2 = document.getElementById('email2').value;
+        let fullEmail = '';
+        const selected = document.getElementById('selectEmail').value;
+        console.log(selected);
+        if(selected === 'direct') {
+            fullEmail = email1 + '@' + email2;
+        } else {
+            fullEmail = email1 + '@' + selected;
+        }
+
+        setUserValue({
+            ...userValue,
+            email: fullEmail
+        });
+        console.log(fullEmail);
+    };
+    //비밀번호 입력 이벤트 핸들러
+    
+    //휴대본번호 입력 이벤트 핸들러
+
+    //주소 입력 이벤트 핸들러
+
+
+
+
+
+
+
 
     //주소검색 입력 변수
     const [enroll_company, setEnroll_company] = useState({
@@ -65,8 +224,9 @@ const Join = () => {
         setPopup(!popup);
     }
 
+        
+        
     
-   
     return (
     
         <>
@@ -74,31 +234,29 @@ const Join = () => {
                 <h3>회원가입</h3>
                 <form action='/login' name='joinForm'>
                     <div className='input-id'>
-                        <input type='text' name='userId' id='userId' placeholder='ID를 입력하세요' />
-                    <button id='idCheckBtn' className='idCheckBtn' type='button'>중복확인</button>    
+                        <input type='text' name='userId' id='userId' placeholder='ID를 입력하세요' onChange={idHandler}/>
+                    <button id='idCheckBtn' className='idCheckBtn' type='button' onClick={idCheck}>중복확인</button>  
+                    <span>{message.userId}</span>  
                     </div>
                     <div className='input-pw'>
                         <input type='password' name='userPw' id='userPw' placeholder='비밀번호를 입력하세요' />
                         <input type='password' name='pwCheck' id='pwCheck' placeholder='비밀번호를 다시 한 번 입력하세요' />
                     </div>
                     <div className='input-nameAndNick'>
-                        <input type='text' name='userName' id='userName' placeholder='이름' />
-                        <input type='text' name='userNick' id='userNick' placeholder='닉네임' />
+                        <input type='text' name='userName' id='userName' placeholder='이름' onClick={nameHandler} />
+                        <input type='text' name='userNick' id='userNick' placeholder='닉네임' onClick={nickHandler}/>
                     </div>
                     <div className='input-email'>
-                        <input type='text' name='email1' id='email1' placeholder='이메일' />
+                        <input type='text' name='email1' id='email1' placeholder='이메일' onChange={email1Handler} />
                         <p>@</p>
-                        <input type='text' name='email2' id='email2' placeholder='직접입력' />
+                        <input type='text' name='email2' id='email2' placeholder='직접입력' onChange={email2Handler} />
 
-                        <select className='selectEmail' name='selectEmail' id='selectEmail'>
+                        <select className='selectEmail' name='selectEmail' id='selectEmail' onChange={selectedEmailHandler}>
                             <option value='direct'>직접입력</option>
                             <option value='gmail.com'>gmail.com</option>
                             <option value='naver.com'>naver.com</option>
                             <option value='hanmail.com'>hanmail.com</option>
                         </select>
-                        <button id='emailCheckBtn' className='emailCheckBtn' type='button'>인증번호받기</button>
-                        <input type='text' name='emailCheckNum' id='emailCheckNum' maxlength="6" placeholder='이메일로 발송된 인증번호 6자리를 입력하세요' />
-                        <button id='emailCheckNumBtn' className='emailCheckNumBtn' type='button'>인증하기</button>
                     </div>
                     <div className='input-phone'>
                         <input type='text' name='phone1' maxlength="3" id='phone1'/>
@@ -116,9 +274,9 @@ const Join = () => {
                         {popup && <KakaoAddress company={enroll_company} setcompany={setEnroll_company}></KakaoAddress>}
                         <input type='text' className='extraAddress' id='extraAddress' placeholder='상세주소' />
                     </div>
-                  
+                
                     <div className='submit-btns'>
-                        <button className="join-check-btn" type="submit">회원가입</button>
+                        <button className="join-check-btn" type="submit" >회원가입</button>
                         <button className='exit-btn'>취소</button>
                     </div>
                     
@@ -129,6 +287,7 @@ const Join = () => {
             
     );
 
-};
+        
+}
 
 export default Join;
