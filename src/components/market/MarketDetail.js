d76677ebb83d3b9eedae05d834f2a291f8db3f32
LeeYoonSeo <diano679@gmail.com>
2023-07-13 20:10:24 +0900

    230713 거래장터 상세보기, 등록하기 틀 완료 / 등록하기 입력값검증 작성중

--- src/components/market/MarketDetail.js
+++ src/components/market/MarketDetail.js
@@ -1,15 +1,84 @@
-import React from 'react'
-import './Market.scss';
-import '../.././Custom.scss';
+import React from "react";
+import "./Market.scss";
+import "../.././Custom.scss";
+import { Box, Button, Grid, Rating } from "@mui/material";
+import { StarBorderRounded } from "@mui/icons-material";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+// 필요 데이터: 상품명, 판매내용, 가격, 이미지번호-(이미지 링크), 등록일, 판매여부
 
 const MarketDetail = () => {
+  const buyBtn = (e) => {
+    //구매버튼
+    console.log("구매하기 버튼 클릭!");
+  };
+
   return (
     <>
-        <div className='container market-detail'>
-        흠
-        </div>
-    </>
-  )
-}
+      <div className="container market-detail">
+        <h1>거래장터</h1>
+        <div className="content">
+          <Grid className="content-img">
+            <article>
+              <img
+                src="https://cdn.mkhealth.co.kr/news/photo/202212/61768_65496_2151.jpg"
+                alt="#"
+              />
+            </article>
+          </Grid>
+          <Grid className="content-content">
+            <Button className="green-btn" variant="contained" disabled>
+              판매 중
+            </Button>
+            <div className="title">
+              <strong>상품명 이름</strong> <span>yyyy.MM.dd</span>
+            </div>
+            <div className="score">
+              <Rating
+                defaultValue={2} //별점데이터
+                size="large"
+                readOnly
+                emptyIcon={<StarBorderRounded fontSize="inherit" />}
+              />
+              <em>2n개 상품평</em>
+            </div>
+            <div className="from">
+              <span>
+                판매자: 누구누구 농부 <br />
+                원산지: ㅇㅇ
+              </span>
+            </div>
+            <div className="text">
+              <p>
+                <em>
+                  ~~에서 키운 맛있는 꿀사과 팝니다. 150자 제한 둬야될듯 Why do
+                  we use it? It is a long established fact that a reader will be
+                  distracted by the readable c
+                </em>
+              </p>
+            </div>
+            <div className="price">
+              <strong>8,600원</strong>
+              <p>우체국 택배 10,000원 이상 무료 배송!</p>
+            </div>
 
-export default MarketDetail
+            <div className="btn-center">
+              <Button
+                className="green-btn center"
+                id="buy-btn"
+                type="button"
+                variant="contained"
+                onClick={buyBtn}
+              >
+                바로 구매 <ChevronRightIcon />
+              </Button>
+            </div>
+          </Grid>
+          {/* content-content END */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MarketDetail;
--- src/components/market/MarketRegist.js
+++ src/components/market/MarketRegist.js
@@ -1,150 +1,214 @@
-import React, {useRef, useState} from 'react'
-import './Market.scss';
-import '../.././Custom.scss';
-import { Button, CssBaseline, TextField, Grid, Box, ThemeProvider, InputAdornment } from '@mui/material';
-import { createTheme } from '@mui/system';
-import { Input } from 'reactstrap';
+import React, { useRef, useState } from "react";
+import "./Market.scss";
+import "../.././Custom.scss";
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  Grid,
+  Box,
+  ThemeProvider,
+  InputAdornment,
+} from "@mui/material";
+import { createTheme } from "@mui/system";
+import { Input } from "reactstrap";
 
 const MarketRegist = () => {
+  const $fileTag = useRef();
+  const [imgFile, setImgFile] = useState(null);
 
-    const $fileTag = useRef();
-    const [imgFile, setImgFile] = useState(null);
+  //클릭 시 이미지 반영하기
+  const showThumbnailHandler = (e) => {
+    const file = $fileTag.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-    const [marketValue, setMarketValue] = useState({
-        marketName: '', price: '', dd: '', email: '', provenance: ''
-    });
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
+  };
 
-    const [priceMsg, setPriceMsg] = useState('');
-    const [correct, setCorrect] = useState({
-        marketName: false, delivery: false, from: false
-    });
+  const [marketValue, setMarketValue] = useState({
+    //전달을 위한 값 저장
+    marketName: null,
+    price: null,
+    info: null,
+    userName: null,
+  });
+  const [correct, setCorrect] = useState({
+    // 검증 통과여부
+    marketName: false,
+    price: false,
+    info: false,
+  });
+  const [priceMsg, setPriceMsg] = useState("숫자만 입력이 가능합니다."); //가격 수정요구 메세지
 
   //검증 데이터를 상태 변수에 저장하는 함수
-  const saveInputState = ({key, inputVal, flag}) => {
-
+  const saveInputState = ({ key, inputVal, flag }) => {
     setMarketValue({
       ...marketValue,
-      [key]: inputVal
+      [key]: inputVal,
     });
 
     setCorrect({
       ...correct,
-      [key]: flag
+      [key]: flag,
     });
+  };
 
-  }
+  //가격 입력마다 입력값 검증
+  const priceHandler = (e) => {
+    const inputVal = e.target.value;
 
+    let flag = false;
+    let msg = " ";
+    if (!/^[0-9]{0,999}$/.test(inputVal)) {
+      msg = "숫자만 입력이 가능합니다.";
+    } else if (!inputVal) {
+      msg = "가격 입력해야된다";
+    } else if (!/^[0-9]{0,6}$/.test(inputVal)) {
+      msg = "상품의 가격은 100만원을 넘길 수 없습니다.";
+    } else {
+      flag = true;
+    }
+    setPriceMsg(msg);
+    setCorrect({ key: "price", inputVal, flag });
+  };
 
+  //상품명 입력값 검증
+  const marketNameHandler = (e) => {
+    const inputVal = e.target.value;
 
-    //클릭 시 이미지 반영하기
-    const showThumbnailHandler = e => {
-        const file = $fileTag.current.files[0];
+    let flag = false;
+    let msg = " ";
+    if (!inputVal) {
+      msg = "빈칸 안됨";
+    } else if (!/^[0-9]{0,6}$/.test(inputVal)) {
+      msg = "상품의 가격은 100만원을 넘길 수 없습니다.";
+    } else {
+      flag = true;
+    }
+    setPriceMsg(msg);
+    setCorrect({ key: "marketName", inputVal, flag });
+  };
 
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
+  // 입력 통과 여부 조회
+  const isValid = () => {
+    for (const c in correct) {
+      const flag = correct[c];
+      if (!flag) return false;
+    }
+    return true;
+  };
 
-        reader.onloadend = () => { setImgFile(reader.result); }
+  //등록하기 버튼 클릭
+  const registBtn = (e) => {
+    console.log("correct" + correct);
+    if (isValid()) {
+      setMarketValue({
+        marketName: document.getElementById("marketNameValue").value,
+        price: document.getElementById("priceValue").value,
+        info: document.getElementById("infoValue").value,
+        userName: document.getElementById("userNameValue").value,
+      });
     }
 
-    //등록하기 버튼 클릭
-    const registBtn = e => {
-        const $price = document.getElementById('priceValue')
-        console.log($price.value);
-        console.log($price);
-    }
-
-    //입력값 검증
-    const priceHandler = e => {
-        const regex = /^[0-9]{2,8}$/;
-        let anPrice = e.target.value;
-        
-        let flag = false;
-        if(!anPrice || regex.test(anPrice)){ //null이거나 숫자가 아닐 경우
-            setCorrect({key: })
-        }
-
-
-    }
-
-
-
-
-
+    console.log(marketValue);
+  };
 
   return (
     <>
-        <div className='container market-regist'>
+      <div className="container market-regist">
         <h1>거래장터</h1>
 
-        <div className='write'>
-            <Grid className='write-image'>
-                <div className='add-file' onClick={() => $fileTag.current.click()}>
-                    <img
-                        src={imgFile || require("../../image/add-image.png")} 
-                        alt="product photo"
-                    />
-                </div>
-                <input id='product-img' type='file'
-                    style={{display: 'none'}}
-                    accept='image/*'
-                    ref={$fileTag}
-                    onChange={showThumbnailHandler}
-                />
-            </Grid>
-
-            <Grid className='write-content'>
-                <div className='answer-content'>
-                    <span>상품명</span>
-                    <TextField variant="standard" color='success'
-                        placeholder='거래할 상품의 이름을 입력해주세요.'
-                    />
-                </div>
-                <div className='answer-content'>
-                    <span>가격</span>
-                    <TextField variant="standard"
-                        id='priceValue'
-                        color='success'
-                        type='number'
-                        placeholder='거래할 상품의 가격을 입력해주세요.'
-                        helperText='숫자만 입력이 가능합니다.'
-                        InputProps={{ 
-                            endAdornment: <InputAdornment position="start" id='price-unit'>원</InputAdornment>,
-                        }}
-                        onChange={priceHandler}
-                    />
-                </div>
-                <div className='answer-content'>
-                    <span>배송</span>
-                    <TextField variant="standard" color='success'
-                        placeholder='배송?'
-                    />
-                </div>
-                <div className='answer-content'>
-                    <span>판매자</span>
-                    <TextField variant="standard" disabled 
-                        value={'판매자명 데이터 가져와서 넣기'}
-                        color='success'
-                    />
-                </div>
-                <div className='answer-content'>
-                    <span>원산지</span>
-                    <TextField variant="standard" color='success'
-                        placeholder='거래할 물품의 원산지를 입력해주세요.'/>
-                </div>
-            </Grid> {/* write-content END */}
+        <div className="write">
+          <Grid className="write-image">
+            <div className="add-file" onClick={() => $fileTag.current.click()}>
+              <img
+                src={imgFile || require("../../image/add-image.png")}
+                alt="product photo"
+              />
+            </div>
+            <input
+              id="product-img"
+              type="file"
+              style={{ display: "none" }}
+              accept="image/*"
+              ref={$fileTag}
+              onChange={showThumbnailHandler}
+            />
+          </Grid>
+          <Grid className="write-content">
+            <div className="answer-content">
+              <span className="list-title">상품명</span>
+              <TextField
+                className="answer-box"
+                variant="standard"
+                id="marketNameValue"
+                color="success"
+                placeholder="상품의 이름을 입력해주세요."
+              />
+            </div>
+            <div className="answer-content">
+              <span className="list-title">가격</span>
+              <TextField
+                className="answer-box"
+                variant="standard"
+                id="priceValue"
+                color="success"
+                type="text"
+                placeholder=" "
+                helperText={priceMsg}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start" id="price-unit">
+                      원
+                    </InputAdornment>
+                  ),
+                }}
+                onChange={priceHandler}
+              />
+            </div>
+            <div className="answer-content">
+              <span className="list-title">상품설명</span>
+              <TextField
+                className="answer-box"
+                variant="outlined"
+                id="infoValue"
+                color="success"
+                multiline
+                rows={4}
+                placeholder="제품 설명을 입력해주세요. (최대 100자)"
+              />
+            </div>
+            <div className="answer-content">
+              <span className="list-title">판매자</span>
+              <TextField
+                className="answer-box"
+                variant="standard"
+                id="userNameValue"
+                disabled
+                value={"판매자 이름 데이터 넣기"}
+                color="success"
+              />
+            </div>
+          </Grid>
+          {/* write-content END */}
         </div>
-
-        <Button className='green-btn' 
-            id='regist-btn' 
-            type="button" 
+        <div className="btn-center">
+          <Button
+            className="green-btn center"
+            id="regist-btn"
+            type="button"
             variant="contained"
-            onClick={registBtn}> 
-        등록하기 
-        </Button>
-
+            onClick={registBtn}
+          >
+            등록하기
+          </Button>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default MarketRegist;
