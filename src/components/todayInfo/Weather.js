42654f9681eea23700baf0e985d563d38337339e
LeeYoonSeo <diano679@gmail.com>
2023-07-18 19:13:54 +0900

    230718 날짜 정보 받아오기 진행중 / 인증키 제외 푸쉬

--- src/components/todayInfo/Weather.js
+++ src/components/todayInfo/Weather.js
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WbSunnySharpIcon from '@mui/icons-material/WbSunnySharp';
 import WbCloudyIcon from '@mui/icons-material/WbCloudy';
 import { json } from "react-router-dom";
+import { ENCODING_KEY, WMCODE_KEY } from '../../config/key-config';
 
 const Weather = () => {
   console.log('날씨 정보창 들어옴'); 
+
+  const [weatherCode, setWeatherCode] = useState('00');
+
+  const [weather0, setWeather0] = useState({});
+  const [weather1, setWeather1] = useState({});
+  const [weather2, setWeather2] = useState({});
+
+
   
-  const $EncodingKey = 'hr0B7fBVo7yq4ogORGVCFWoku%2FrF2GHMmVivMJtANj3xiGOQ2%2BJ3Pc5Di6oBflSCiBGmBjbWjw1ZyZpE4WCZVQ%3D%3D'; //인증키
-  const $areaId = '4822000000'; //지역 아이디 (엑셀 참고)
-  const $speId = 'PA999999'; //작물별 특성 아이디
-  const $stYmd = getDate(0).dateString; //오늘 날짜
-
-  fetch('http://apis.data.go.kr/1360000/FmlandWthrInfoService/getDayStatistics?serviceKey='+$EncodingKey+'&numOfRows=10&pageNo=1&dataType=JSON&ST_YMD='+$stYmd+'&ED_YMD=20161201&AREA_ID='+$areaId+'&PA_CROP_SPE_ID=PA999999'
-  ).then(res => res.json())
-  .then(data => {
-    console.log(data);
-    console.log(data.response.header);
-  })
-
-
   //날짜 정보 구하기
   function getDate(plusDay) {
 
@@ -38,6 +34,55 @@ const Weather = () => {
     return {dateString, month, day, dayName};
   }
 
+  // //주산지 특보 조회
+  // fetch('http://apis.data.go.kr/1360000/FmlandWthrInfoService/getDayStatistics?serviceKey='+WMCODE_KEY
+  //     +'&numOfRows=10&pageNo=1&dataType=JSON&ST_YMD='+$stYmd
+  //     +'&ED_YMD=20161201&AREA_ID='+$areaId
+  //     +'&PA_CROP_SPE_ID='+$speId
+  // ).then(res => res.json())
+  // .then(data => {
+  //   console.log('주산지 특보 조회'+data);
+  //   console.log(data.response.header);
+  // })
+
+
+  const [temps, setTemps] = useState({})
+
+    //일통계 조회
+    const getTempData = (plusDay) => {
+
+      const $stnIds = '108'; //지역 아이디 워드파일 참고 (108: 서울)
+      const $stYmd = getDate(-plusDay).dateString; //오늘 날짜
+      
+      fetch('http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey='+ENCODING_KEY
+          +'&numOfRows=10&pageNo=1&dataType=JSON&dataCd=ASOS&dateCd=DAY&startDt='+$stYmd+'&endDt='+$stYmd+'&stnIds='+$stnIds
+      ).then(res => res.json())
+      .then(data => {
+        console.log('응답메시지 코드: '+weatherCode);
+        console.log('받아온 정보: ', data);
+
+        setWeatherCode(data.response.header.resultCode)
+        const datas = data.response.body.items.item[0];
+        setTemps({
+          avgTa : datas.avgTa, //평균온도
+          minTa : datas.minTa, //최저온도
+          maxTa : datas.maxTa, //최고온도
+          iscs : datas.iscs   
+        })
+      })
+
+      return {temps}
+    }
+
+
+  useEffect(() => {
+    console.log('Effect 작동');
+
+    setWeather0(getTempData(3).temps);
+    setWeather1(getTempData(2).temps);
+    setWeather2(getTempData(1).temps);
+
+  }, []) //effect 종료
 
 
   return(
@@ -54,7 +99,7 @@ const Weather = () => {
                   </div>
                 </div>
                 <div className='temp'>
-                  <span>25℃</span> <hr/> <span>28℃</span>
+                  <span>{weather0.minTa}℃</span> <hr/> <span>{weather0.maxTa}℃</span>
                 </div>
               </div> {/* weather D-0 END */}
               
@@ -67,7 +112,7 @@ const Weather = () => {
                   </div>
                 </div>
                 <div className='temp'>
-                  <span>25℃</span> <hr/> <span>28℃</span>
+                <span>{weather1.minTa}℃</span> <hr/> <span>{weather1.maxTa}℃</span>
                 </div>
               </div> {/* weather D+1 END */}
 
@@ -80,7 +125,7 @@ const Weather = () => {
                   </div>
                 </div>
                 <div className='temp'>
-                  <span>25℃</span> <hr/> <span>28℃</span>
+                <span>{weather2.minTa}℃</span> <hr/> <span>{weather2.maxTa}℃</span>
                 </div>
               </div> {/* weather D+2 END */}
 
