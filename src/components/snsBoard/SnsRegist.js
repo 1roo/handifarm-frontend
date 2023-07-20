540e1c7fdb5383948ea5892cd622bf6139cc82d2
1roo <93hanaru@gmail.com>
2023-07-21 01:19:54 +0900

    230721 SnsRegist 모달로 틀 구현

--- src/components/snsBoard/SnsRegist.js
+++ src/components/snsBoard/SnsRegist.js
@@ -1,93 +1,96 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Container } from "@mui/material";
-import "./Sns.scss";
-import addImage from "../../image/add-image.png";
+import React, { useState } from 'react';
+import axios from 'axios';
+import registImg from '../../image/add-image.png';
+import { Container } from "react-bootstrap";
 
-const SnsRegist = ({ onRequestClose }) => {
-  const [formData, setFormData] = useState({
-    content: "",
-    fileUp: null,
-    hashTags: "",
-    previewUrl: null, // 추가: 파일 미리보기 URL 저장
-  });
+const SnsRegist = () => {
+  const [content, setContent] = useState('');
+  const [hashTags, setHashTags] = useState([]);
+  const [itemImgs, setItemImgs] = useState([]);
+  const [imagePreview, setImagePreview] = useState(null); // 이미지 미리보기를 위한 상태
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setFormData({ ...formData, fileUp: file });
+    try {
+      const formData = new FormData();
+      formData.append('content', content);
 
-    // 파일 미리보기 생성
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setFormData({ ...formData, previewUrl: reader.result });
-      };
-      reader.readAsDataURL(file);
-    } else {
-      setFormData({ ...formData, previewUrl: null });
+      hashTags.forEach((tag) => formData.append('hashTags', tag));
+      itemImgs.forEach((img) => formData.append('itemImgs', img));
+
+      const response = await axios.post('/api/cboard', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data);
+      // 성공 시 응답 데이터 처리 또는 다른 페이지로 리다이렉션 처리
+    } catch (error) {
+      console.error('폼 제출 중 에러 발생:', error);
+      // 에러 처리 또는 사용자에게 에러 메시지 표시
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const { content, fileUp, hashTags } = formData;
-
-    // FormData를 사용하여 파일 업로드를 위한 데이터 전송
-    const formDataToSend = new FormData();
-    formDataToSend.append("content", content);
-    formDataToSend.append("fileUp", fileUp);
-    formDataToSend.append("hashTags", hashTags);
-
-    // 서버로 데이터 전송
-    axios
-      .post("/api/cboard", formDataToSend) // 실제 API 엔드포인트로 변경해야 합니다.
-      .then((response) => {
-        console.log("게시글 등록 완료:", response.data);
-        onRequestClose(); // 등록 성공 시 모달 닫기
-      })
-      .catch((error) => {
-        console.error("게시글 등록 실패:", error);
-      });
+  const handleFileInputChange = (e) => {
+    setItemImgs(e.target.files);
+    // 이미지 미리보기를 위해 선택한 이미지 파일의 URL을 생성
+    const imageFile = e.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setImagePreview(imageUrl);
   };
 
   return (
-    <Container component="main" maxWidth="sm">
-      <div>
-        <p className="menu-title">게시글 등록</p>
-        <form onSubmit={handleSubmit}>
-          <div className="photo-upload">
-            {formData.previewUrl ? (
-              <img src={formData.previewUrl} alt="미리보기" />
-            ) : (
-              <label htmlFor="fileUp">
-                <img src={addImage} alt="사진 업로드" />
-              </label>
-            )}
+    <Container maxwidth="300px">
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          {imagePreview ? (
+            <img
+              src={imagePreview}
+              alt="이미지 미리보기"
+              style={{ width: '200px', height: '200px', margin: '0 40px 20px' }}
+            />
+          ) : (
+            <label htmlFor="itemImgs" style={{ cursor: 'pointer' }}>
+              <img
+                src={registImg}
+                alt="이미지 아이콘"
+                style={{ width: '200px', height: '200px', margin: '0 40px 20px' }}
+              />
+            </label>
+          )}
+          <input
+            type="file"
+            id="itemImgs"
+            onChange={handleFileInputChange}
+            multiple
+            style={{ display: 'none' }}
+          />
+        </div>
+        <div className='regist-content'>
+          <div>
+            <label htmlFor="content">내용:</label>
             <input
-              type="file"
-              id="fileUp"
-              name="fileUp"
-              onChange={handleFileChange}
-              style={{ display: "none" }}
+              type="text"
+              id="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              required
             />
           </div>
           <div>
-            <label htmlFor="content">내용:</label>
-            <input type="text" name="content" onChange={handleChange} required />
-          </div>
-          <div>
             <label htmlFor="hashTags">해시태그:</label>
-            <input type="text" name="hashTags" onChange={handleChange} />
+            <input
+              type="text"
+              id="hashTags"
+              value={hashTags.join(',')}
+              onChange={(e) => setHashTags(e.target.value.split(','))}
+            />
           </div>
-          <button type="submit">등록</button>
-        </form>
-      </div>
+        </div>
+        <button className='regist-btn' type="submit">등록</button>
+      </form>
     </Container>
   );
 };
