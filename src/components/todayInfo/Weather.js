38fe6bc112a3d3158858ce7510ab894ddc5333bb
LeeYoonSeo <diano679@gmail.com>
2023-07-28 09:59:52 +0900

    230728 오늘의 정보 날씨 데이터 로딩 후 출력부분 수정

--- src/components/todayInfo/Weather.js
+++ src/components/todayInfo/Weather.js
@@ -25,8 +25,14 @@ import { ENCODING_KEY, WMCODE_KEY } from '../../config/key-config';
 
 const Weather = () => {
 
+  const [loading, setLoading] = useState(true);
+
+  const [stateTemp, setStateTemp] = useState()
+  const [stateSkyList, setStateSkyList] = useState()
+
   const [weatherIcon, setWeatherIcon] = useState([<WbSunnySharpIcon />, <WbCloudyIcon />, <UmbrellaIcon />, <AcUnitIcon />]);
   // const [weatherIconNum, setWeatherIconNum] = useState([0,0,0,0,0,0]);
+
 
 
   //날짜 정보 구하기
@@ -49,115 +55,111 @@ const Weather = () => {
     return {dateString, month, day, time, dayName};
   }
 
+  useEffect(async () => {
 
-  //일통계 조회
-  const getTempData = async (plusDay) => {
-
-    const stYmd = getDate(+plusDay).dateString; //오늘 날짜
-    const stYesterdayYmd = getDate(+(plusDay-1)).dateString; //어제 날짜 확인용 (오늘)
-    // const $stTime = (getDate(-plusDay).time-1)+'00';
-    const stTime = '0500';
-    let nX = '59';
-    let nY = '126';
+    //일통계 조회
     
-    console.log('\n\n 오늘 날짜: '+stYmd);
-    
-    
-    //실황
-    // const resRealTime = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey='+ENCODING_KEY
-    //  +'&numOfRows=10&pageNo=1&dataType=JSON&base_date='+stYmd+'&base_time='+stTime+'&nx='+nX+'&ny='+nY)
-    // const data = await resRealTime.json();
-    // console.log('data: ', data);
-    // const temp = data.response.body.items.item[3].obsrValue;
-    // console.log('현재기온: ', temp);
-    // const dd = document.querySelector('.weather.D0 > .temp > span');
-    // dd.textContent = temp+'℃';
 
-
-    //단기예보 
-    const resWeather = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey='+ENCODING_KEY
-      +'&numOfRows=1000&pageNo=1&dataType=JSON&base_date='+stYmd+'&base_time='+stTime+'&nx='+nX+'&ny='+nY);
-    const resYesterWeather = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey='+ENCODING_KEY
-      +'&numOfRows=400&pageNo=1&dataType=JSON&base_date='+stYesterdayYmd+'&base_time='+stTime+'&nx='+nX+'&ny='+nY);
+      const stYmd = getDate(0).dateString; //오늘 날짜
+      const stYesterdayYmd = getDate(-1).dateString; //어제 날짜 확인용 (오늘)
+      // const $stTime = (getDate(-plusDay).time-1)+'00';
+      const stTime = '0500';
+      let nX = '59';
+      let nY = '126';
       
-    const data = await resWeather.json();
-    const data2 = await resYesterWeather.json();
+      console.log('\n\n 오늘 날짜: '+stYmd);
+      
+      
+      //실황
+      // const resRealTime = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey='+ENCODING_KEY
+      //  +'&numOfRows=10&pageNo=1&dataType=JSON&base_date='+stYmd+'&base_time='+stTime+'&nx='+nX+'&ny='+nY)
+      // const data = await resRealTime.json();
+      // console.log('data: ', data);
+      // const temp = data.response.body.items.item[3].obsrValue;
+      // console.log('현재기온: ', temp);
+      // const dd = document.querySelector('.weather.D0 > .temp > span');
+      // dd.textContent = temp+'℃';
 
-    if(data.response.header.resultCode !== '00'){ //정상처리되지 않았을 경우
-      console.log('잘못된 요청입니다.');
-      // return;
-    }
 
-    const itemList = data.response.body.items.item;
-    const itemList2 = data2.response.body.items.item;
+      //단기예보 
+      const resWeather = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey='+ENCODING_KEY
+        +'&numOfRows=1000&pageNo=1&dataType=JSON&base_date='+stYmd+'&base_time='+stTime+'&nx='+nX+'&ny='+nY);
+      const resYesterWeather = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey='+ENCODING_KEY
+        +'&numOfRows=400&pageNo=1&dataType=JSON&base_date='+stYesterdayYmd+'&base_time='+stTime+'&nx='+nX+'&ny='+nY);
+        
+      const data = await resWeather.json();
+      const data2 = await resYesterWeather.json();
 
-    // 최고, 최저온도 구하기
-    const temps = itemList.filter(it => it.category == 'TMN' || it.category == 'TMX');
-    const temp = temps.map(it => it.fcstValue);
-    itemList2.forEach(it => {  //오늘 최저기온 구하기 1
-      if(it.category == 'TMN'){
-        temp.unshift(it.fcstValue);
+      if(data.response.header.resultCode !== '00'){ //정상처리되지 않았을 경우
+        console.log('잘못된 요청입니다.');
         return;
       }
-    });
 
-    // 사흘간 날씨 구하기 (오전 오후 6시 기준)
-    const skys = itemList.filter(it => it.category == 'SKY' && (it.fcstTime == '0600' || it.fcstTime == '1800' ))
-    const skyCode = skys.map(it => it.fcstValue);
+      const itemList = data.response.body.items.item;
+      const itemList2 = data2.response.body.items.item;
 
-    //사흘간 날씨 구하기22 (오전 오후 6시 기준)
-    const rains = itemList.filter(it => it.category == 'PTY' && (it.fcstTime == '0600' || it.fcstTime == '1800' ))
-    const rainCode = rains.map(it => it.fcstValue);
+      // 최고, 최저온도 구하기
+      const temps = itemList.filter(it => it.category == 'TMN' || it.category == 'TMX');
+      const temp = temps.map(it => it.fcstValue);
+      itemList2.forEach(it => {  //오늘 최저기온 구하기 1
+        if(it.category == 'TMN'){
+          temp.unshift(it.fcstValue);
+          return;
+        }
+      });
+
+      // 사흘간 날씨(구름 소식) 구하기 (오전 오후 6시 기준)
+      const skys = itemList.filter(it => it.category == 'SKY' && (it.fcstTime == '0600' || it.fcstTime == '1800' ))
+      const skyCode = skys.map(it => it.fcstValue);
+
+      //사흘간 날씨(비 소식) 구하기22 (오전 오후 6시 기준)
+      const rains = itemList.filter(it => it.category == 'PTY' && (it.fcstTime == '0600' || it.fcstTime == '1800' ))
+      const rainCode = rains.map(it => it.fcstValue);
 
 
-    // 맑음0, 흐림1, 비2, 눈3, 
-    const skyRainList = []
-    let count = 0;
-    skyCode.forEach(sc => {
-      let state = 0 //상태저장
-      if(sc == 3 || sc == 4){ state = 1 }
+
+      // 맑음0, 흐림1, 비2, 눈3으로 숫자 통일
+      const skyRainList = []
+      let count = 0;
+      skyCode.forEach(sc => {
+        let state = 0 //상태저장
+        if(sc == 3 || sc == 4){ state = 1 }
+        
+        if(rainCode[count] == 3 || rainCode[count] == 7) state = 3;
+        else if(rainCode[count] > 0) state = 2;
+
+        skyRainList.push(state);
+        count++;
+      });
+
+      console.log('날씨코드:', skyCode, '비 여부 코드:', rainCode);
+      console.log('어제, 오늘, 내일 날씨 코드: ', skyRainList);
+      console.log('어제, 오늘, 내일의 최저/최고 온도: ', temp);
+
+      setStateSkyList(skyRainList);
+      setStateTemp(temp);
       
-      if(rainCode[count] == 3 || rainCode[count] == 7) state = 3;
-      else if(rainCode[count] > 0) state = 2;
+      // localStorage.setItem('tempData', temp);
+      // localStorage.setItem('skyData', skyRainList);
+      // console.log(localStorage.getItem('tempData'));
+      // console.log(localStorage.getItem('skyData'));
 
-      skyRainList.push(state);
-      count++;
-    });
-    // setWeatherIcon(skyRainList)
+    //로딩 완료
+    setLoading(false)
 
+  }, [])
 
-    console.log('날씨코드:', skyCode, '비 여부 코드:', rainCode);
-    console.log('어제, 오늘, 내일 날씨 코드: ', skyRainList);
-    console.log('어제, 오늘, 내일의 최저/최고 온도: ', temp);
-
-    // localStorage.setItem('tempData', temp);
-    // localStorage.setItem('skyData', skyRainList);
-    // console.log(localStorage.getItem('tempData'));
-    // console.log(localStorage.getItem('skyData'));
-
-    document.querySelector('.weather.D0 .temp span:nth-of-type(1)').textContent = temp[0]+'℃';
-    document.querySelector('.weather.D0 .temp span:nth-of-type(2)').textContent = temp[1]+'℃';
-    document.querySelector('.weather.D1 .temp span:nth-of-type(1)').textContent = temp[2]+'℃';
-    document.querySelector('.weather.D1 .temp span:nth-of-type(2)').textContent = temp[3]+'℃';
-    document.querySelector('.weather.D2 .temp span:nth-of-type(1)').textContent = temp[4]+'℃';
-    document.querySelector('.weather.D2 .temp span:nth-of-type(2)').textContent = temp[5]+'℃';
-
-    // 바로바로 출력은 되나 텍스트 Object [Object] 이따구로 불러와짐.
-    // document.querySelector('.weather.D0 div .icon i:nth-of-type(1)').innerHTML = weatherIcon[skyRainList[0]];
-    // document.querySelector('.weather.D0 div .icon i:nth-of-type(2)').innerHTML = weatherIcon[skyRainList[1]];
-    // document.querySelector('.weather.D1 div .icon i:nth-of-type(1)').textContent = weatherIcon[skyRainList[2]];
-    // document.querySelector('.weather.D1 div .icon i:nth-of-type(2)').textContent = weatherIcon[skyRainList[3]];
-    // document.querySelector('.weather.D2 div .icon i:nth-of-type(1)').textContent = weatherIcon[skyRainList[4]];
-    // document.querySelector('.weather.D2 div .icon i:nth-of-type(2)').textContent = weatherIcon[skyRainList[5]];
-
-  }
-
+  const loadingPage = (
+    <div id="loading">
+      <h2>로딩창 로고 이용한 gif로 교체하고싶다</h2>
+      <img id="loadingElement" 
+          src="https://cdn-icons-png.flaticon.com/512/189/189768.png"/>
+    </div>   
+  )
 
   return(
   <>
-    {/* <div id="loading">
-        <img id="loadingElement" src="https://cdn-icons-png.flaticon.com/512/189/189768.png"/>
-    </div>    */}
+    { loading ? loadingPage : 
     <section className='weather-box'>
         <div className='title'><h2>서울<br/>날씨</h2></div>
 
@@ -165,12 +167,12 @@ const Weather = () => {
           <div>
             <div className='day'><h3>오늘</h3><span>{getDate(0).month}/{getDate(0).day} {getDate(0).dayName}</span></div>
             <div className='icon'>
-              <span className='am'>오전</span> <i>{weatherIcon[0]}</i>
-              <span className='pm'>오후</span> <i>{weatherIcon[1]}</i>
+              <span className='am'>오전</span> <i>{weatherIcon[stateSkyList[0]]}</i>
+              <span className='pm'>오후</span> <i>{weatherIcon[stateSkyList[1]]}</i>
             </div>
           </div>
           <div className='temp'>
-            <span>{getTempData(0).temp}22℃</span> <hr/> <span>29℃</span>
+            <span>{stateTemp[0]}℃</span> <hr/> <span>{stateTemp[1]}℃</span>
           </div>
         </div> {/* weather D-0 END */}
         
@@ -178,12 +180,12 @@ const Weather = () => {
           <div>
             <div className='day'><h3>내일</h3><span>{getDate(1).month}/{getDate(1).day} {getDate(1).dayName}</span></div>
             <div className='icon'>
-              <span className='am'>오전</span>  <i>{weatherIcon[2]}</i>
-              <span className='pm'>오후</span>  <i>{weatherIcon[3]}</i>
+              <span className='am'>오전</span>  <i>{weatherIcon[stateSkyList[2]]}</i>
+              <span className='pm'>오후</span>  <i>{weatherIcon[stateSkyList[3]]}</i>
             </div>
           </div>
           <div className='temp'>
-            <span>25℃</span> <hr/> <span>33℃</span>
+            <span>{stateTemp[2]}℃</span> <hr/> <span>{stateTemp[3]}℃</span>
           </div>
         </div> {/* weather D+1 END */}
 
@@ -191,16 +193,17 @@ const Weather = () => {
           <div>
             <div className='day'><h3>모레</h3><span>{getDate(2).month}/{getDate(2).day} {getDate(2).dayName}</span></div>
             <div className='icon'>
-              <span className='am'>오전</span> <i>{weatherIcon[0]}</i>
-              <span className='pm'>오후</span> <i>{weatherIcon[0]}</i>
+              <span className='am'>오전</span> <i>{weatherIcon[stateSkyList[4]]}</i>
+              <span className='pm'>오후</span> <i>{weatherIcon[stateSkyList[5]]}</i>
             </div>
           </div>
           <div className='temp'>
-            <span>24℃</span> <hr/> <span>31℃</span>
+            <span>{stateTemp[4]}℃</span> <hr/> <span>{stateTemp[5]}℃</span>
           </div>
         </div> {/* weather D+2 END */}
 
     </section>
+    }
   </>
   )
 };
