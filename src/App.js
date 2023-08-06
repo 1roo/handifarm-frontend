4f3d620ea1ec187cc940a9e9131c61a93372f700
flatcalm <125535629+flatcalm@users.noreply.github.com>
2023-08-06 16:15:29 +0900

    Revert "Feat/eg -> develop Merging 충돌 해결"

--- src/App.js
+++ src/App.js
@@ -1,87 +1,26 @@
-import "./App.css";
-import "./Custom.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { Reset } from "styled-reset";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import Home from "./components/Home";
-import Login from "./components/user/Login";
-import Join from "./components/user/Join";
-import Mypage from "./components/user/Mypage";
-import BoardList from "./components/board/BoardList";
-import BoardRegist from "./components/board/BoardRegist";
-import BoardDetail from "./components/board/BoardDetail";
-import BoardModify from "./components/board/BoardModify";
-import BoardReply from "./components/board/BoardReply";
-import { AuthContextProvider } from "./components/util/AuthContext";
-import MarketList from "./components/market/MarketList";
-import MarketDetail from "./components/market/MarketDetail";
-import MarketRegist from "./components/market/MarketRegist";
-import MarketModify from "./components/market/MarketModify";
-import SnsList from "./components/snsBoard/SnsList";
-import SnsRegist from "./components/snsBoard/SnsRegist";
-import SnsDetail from "./components/snsBoard/SnsDetail";
-import SnsUserDetail from "./components/snsBoard/SnsUserDetail";
-import Weather from "./components/todayInfo/Weather";
-import Pest from "./components/todayInfo/Pest";
-import Payment from "./components/payment/Payment";
-import { PaymentSuccess } from "./components/payment/PaymentSuccess";
-import { PaymentFail } from "./components/payment/PaymentFail";
-import Error404 from "./components/Error404";
-import TodayInfoPage from "./components/todayInfo/TodayInfoPage";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './components/Home';
+import Login from './components/user/Login';
+import Join from './components/user/Join';
 
-function App(props) {
+
+function App() {
   return (
-    <AuthContextProvider>
-      <Router>
-        <Reset />
-        <Header />
+    <Router>
+      <Header />
 
         <Routes>
-          <Route exact path="/" element={<Home />} />
-
-          {/* >> User (회원가입, 로그인, 마이페이지) */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/mypage" element={<Mypage />} />
-
-          {/* >> Board 게시판 */}
-          <Route path="/board" element={<BoardList />} />
-          <Route path="/boardRegist" element={<BoardRegist />} />
-          <Route path="/board/:boardNo" element={<BoardDetail />} />
-          <Route path="/boardModify/:boardNo" element={<BoardModify />} />
-          <Route path="/board/:boardNo/boardReply" element={<BoardReply />} />
-
-          {/* >> Market 거래장터 */}
-          <Route path="/market" element={<MarketList />} />
-          <Route path="/marketRegist" element={<MarketRegist />} />
-          <Route path="/marketDetail" element={<MarketDetail />} />
-          <Route path="/marketModify" element={<MarketModify />} />
-
-          {/* >> SNS Board 농사일기 */}
-          <Route path="/snsBoard" element={<SnsList />} />
-          <Route path="/snsBoard/snsRegist" element={<SnsRegist />} />
-          <Route path="/snsBoard/:writer" element={<SnsUserDetail />} />
-          <Route path="/snsBoard/:snsNo/:writer" element={<SnsDetail />} />
-
-          {/* >> TodayInfo오늘의 정보 */}
-          <Route path="/weather" element={<Weather />} />
-          <Route path="/todayInfo" element={<TodayInfoPage />} />
-          <Route path="/pest" element={<Pest />} />
-
-          {/* 기타 */}
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/success" element={<PaymentSuccess />} />
-          <Route path="/fail" element={<PaymentFail />} />
-
-          {/* >> 404 에러 처리 >>  */}
-          <Route path="/*" element={<Error404 />} />
+          <Route exact path="/" element={ <Home /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/join" element={ <Join /> } />
         </Routes>
+        
 
-        <Footer />
-      </Router>
-    </AuthContextProvider>
+      <Footer />  
+    </Router>
   );
 }
 
--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,215 +1,88 @@
-import React, { useContext, useEffect, useState } from "react";
-import "./Login.scss";
-import kakaoImg from "../../image/kakao.png";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-import { Link, useNavigate } from "react-router-dom";
-import { API_BASE_URL as BASE, USER } from "../../config/host-config";
-import AuthContext from "../util/AuthContext";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
+import kakaoImg from '../../image/kakao.png';
+import naverImg from '../../image/naver.png';
 
 const Login = () => {
-  const redirection = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectedSns, setSelectedSns] = useState('');
 
-  console.log("AuthContext!에서 onLogin 가져옴");
-
-  const { onLogin, isLoggedIn } = useContext(AuthContext);
-  console.log("isLoggedIn: ", isLoggedIn);
-
-  useEffect(() => {
-    if (localStorage.getItem("ACCESS_TOKEN")) {
-      console.log("isLoggedIn True!");
-      setTimeout(() => {
-        redirection("/");
-      }, 3000);
-    }
-  }, []);
-
-  const REQUEST_URL = BASE + USER + "/login";
-
-  const fetchLogin = async () => {
-    //사용자가 입력한 아이디, 비밀번호 입력 태그 얻어오기
-    const $userId = document.getElementById("userId");
-    const $userPw = document.getElementById("userPw");
-
-    const res = await fetch(REQUEST_URL, {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        userId: $userId.value,
-        userPw: $userPw.value,
-      }),
-    });
-    console.log("login fetch complete!");
-
-    if (res.status === 400) {
-      const text = await res.text();
-      alert(text);
-      return;
-    }
-
-    const { userId, userName, userNick, userProfileImg, token } =
-      await res.json();
-
-    // Context API를 사용하여 로그인 상태를 업데이트합니다.
-    onLogin(token, userId, userName, userNick, userProfileImg);
-
-    //홈으로 리다이렉트
-    redirection("/");
-  };
-
-  //로그인 요청 핸들러
-  const loginHandler = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-
-    // 서버에 로그인 요청 전송
-    fetchLogin();
+    setShowModal(true);
   };
+  const closeModal = () => {
+    setShowModal(false);
+  }
 
-  const defaultTheme = createTheme();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      userId: data.get("userId"),
-      userPw: data.get("userPw"),
-    });
-  };
-
-  //Kakao Login
-  const Rest_api_key = "38942666792fea4c80c24cdac16be341"; //REST API KEY
-  const redirect_uri = "http://localhost:3000/api/user/auth"; //Redirect URI
-  // auth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-  const handleLogin = () => {
-    window.open(kakaoURL, "카카오 로그인", "width=500,height=600");
-  };
-
-  //Naver Login
-  const { naver } = window;
-  const naverLogin = new naver.LoginWithNaverId({
-    clientId: "0rgKKooFAROCjSXfGI_2",
-    callbackUrl: "http://localhost:3000/login",
-    isPopup: true,
-    loginButton: {
-      color: "green",
-      type: 1,
-      height: 50,
-    },
-  });
-
-  useEffect(() => {
-    naverLogin.init();
-    console.log("네이버init!");
-  }, []);
-
-  const [user, setUser] = useState(null);
-
-  const getUser = async () => {
-    await naverLogin.getLoginStatus((status) => {
-      console.log(`로그인?: ${status}`);
-      if (status) {
-        setUser({ ...naverLogin.user });
-        window.opener.location.href = "http://localhost:3000";
-        window.close();
-      }
-    });
-  };
-
-  const redirect = (e) => {
-    redirection("/");
+  const handleSnsClick = (sns) => {
+    setSelectedSns(sns);
+    setShowModal(true);
   };
 
   return (
     <>
-      <p className="menu-title">로그인</p>
-      <ThemeProvider theme={defaultTheme}>
-        <Container id="login-coniatiner" component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="userId"
-                label="아이디"
-                name="userId"
-                autoComplete="userId"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="userPw"
-                label="비밀번호"
-                type="password"
-                id="userPw"
-                autoComplete="current-password"
-              />
-              <Grid container spacing={2}>
-                <Grid item xs={6} sm={6}>
-                  <Button
-                    className="loginBtn"
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    onClick={loginHandler}
-                  >
-                    로그인
-                  </Button>
-                </Grid>
-                <Grid item xs={6} sm={6}>
-                  <Button
-                    className="loginExitBtn"
-                    type="button"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    onClick={redirect}
-                  >
-                    취소
-                  </Button>
-                </Grid>
-              </Grid>
-              <form>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={12}>
-                    <div className="sns-box">
-                      <p>SNS 로그인</p>
-                      <div className="sns-list">
-                        <img src={kakaoImg} onClick={handleLogin} />
-                        <div id="naverIdLogin"></div>
-                      </div>
-                    </div>
-                  </Grid>
-                </Grid>
-              </form>
-            <div id="link-join-question"><Link to="/join">회원이 아니신가요?</Link></div>
-            </Box>
-          </Box> {/* column BOX END */}
-        </Container>
-      </ThemeProvider>
-    </>
+      <div className='container'>
+        <div className='inner-container'>
+          <h3>로그인</h3>
+          <form>
+            <div className='login-box'>
+              <div className='login-input'>
+                <input type="text" id="username" value={username} placeholder='ID를 입력하세요' />
+                <input type="password" id="password" value={password} placeholder='비밀번호를 입력하세요'/>
+              </div>
+              <button className="login-btn" type="submit">로그인</button>
+            </div>
+            <div className='join-box'>
+              <div className='span-div'>
+                <span>아직 계정이 없으세요?</span>
+                <span>지금 바로 회원가입 해 보세요!</span>
+              </div>
+              <Link to='/login' className='button-link join-btn'>
+                회원가입
+              </Link>
+            </div>
+            </form>
+          </div>
+          
+          <form>
+          <div className='sns-box'>
+            <p>SNS 로그인</p>
+            <div className='sns-list'>
+              <img src={kakaoImg} id='kakao' className='sns-select kakao' onClick={() => handleSnsClick('kakao')} />
+              <img src={naverImg} id='naver' className='sns-select naver' onClick={() => handleSnsClick('naver')} />
+            </div>
+          </div>
+        </form>
+      
+
+
+      {showModal && selectedSns === 'kakao' && (
+        <div className="modal-container">
+          <div className="modal">
+            <h3>카카오로그인</h3>
+            <p>아이디: {username}</p>
+            <p>비밀번호: {password}</p>
+            <button onClick={closeModal}>닫기</button>
+          </div>
+        </div>
+      )}
+
+      {showModal && selectedSns === 'naver' && (
+        <div className="modal-container">
+          <div className="modal">
+            <h3>네이버로그인</h3>
+            <p>아이디: {username}</p>
+            <p>비밀번호: {password}</p>
+            <button onClick={closeModal}>닫기</button>
+          </div>
+        </div>
+      )}
+      </div>
+    </>  
   );
 };
 
