4ee2d2b664fb4badd3c1a0497ae041b025a245f9
Issac0126 <125543907+Issac0126@users.noreply.github.com>
2023-08-06 17:51:52 +0900

    230806 App.js에서 reset 빼지않기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "./Custom.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Reset } from "styled-reset";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
